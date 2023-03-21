e6ea558fa2c90197c5cdda2086c2f4c2c6db20b9
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-03-21 13:18:59 +0300

    сортировка исправлена, добавила обработчик ошибок

--- components/Tbody.js
+++ components/Tbody.js
@@ -1,61 +1,48 @@
 import s from './Tbody.module.css';
 import { useEffect, useState } from 'react';
 
+
 function Tbody() {
   const [users, setUsers] = useState([]);
+  // const [order, setOrder] = useState('asc'); // все useState подряд и вместе 
 
-  const getUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const FinalData = await response.json();
-    setUsers(FinalData)
-  }
-
-  useEffect(() => {
-    getUsers();
-  }, [])
-
-
-  const [order, setOrder] = useState('ASC');
-  const sorting = (obj, key) => {
-    if (order === "ASC") {
-      const sorted = [...users].sort((a, b) =>
-        a[obj] > b[obj] ? 1 : -1
-      );
-      setUsers(sorted)
-      setOrder('DSC')
+    const getUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const FinalData = await response.json();
+      setUsers(FinalData)
     }
-    if (order === 'DSC') {
-      const sorted = [...users].sort((a, b) =>
-        a[obj] < b[obj] ? 1 : -1
-      );
-      setUsers(sorted)
-      setOrder('ASC')
-    }
-  }
+
+    useEffect(() => { //извлечение данных, ручное изменение структуры DOM, использование таймеров, логгирование
+      getUsers();
+    }, []);
+
+ 
+  function sortByFn(fn){
+  setUsers([...users].sort((a,b)=>fn(a).localeCompare(fn(b))))
+}
+
   return <>
     <thead className={s.thead}>
       <tr>
         <th>
-          <button onClick={() => sorting('name')}>
-            Name</button></th>
-        <th><button onClick={() => sorting('email')}>
+          <button onClick={() => sortByFn(obj=>obj.name)}>
+          Name</button></th>
+        <th><button onClick={() => sortByFn(obj=>obj.email)}>
           Email</button></th>
-        <th><button onClick={() => sorting('address.city')}>
+        <th><button onClick={() => sortByFn(obj=>obj.address.city)}>
           Address.city</button></th>
-        <th><button onClick={() => sorting('phone')}>
+        <th><button onClick={() => sortByFn(obj=>obj.phone)}>
           Phone</button></th>
-        <th><button onClick={() => sorting('website')}>
+        <th><button onClick={() => sortByFn(obj=>obj.website)}>
           Website</button></th>
-        <th><button onClick={() => sorting('company.name')}>
+        <th><button onClick={() => sortByFn(obj=>obj.company.name)}>
           Company.Name</button></th>
       </tr>
     </thead>
+   <tbody className={s.tbody}>
 
-    <tbody className={s.tbody}>
-
-      {users.map((userElem) => {
-        return <>
-          <tr key={userElem.id}>
+    {users.map((userElem) => {
+        return <tr key={userElem.id}>
             <td>{userElem.name}</td>
             <td>{userElem.email}</td>
             <td>{userElem.address.city}</td>
@@ -63,8 +50,7 @@ function Tbody() {
             <td>{userElem.website}</td>
             <td>{userElem.company.name}</td>
           </tr>
-        </>
-      })
+        })
       }
     </tbody>
   </>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Header from '../components/Header';
 import Tbody from '../components/Tbody';
 import s from './Index.module.css';
-
-
+import {ErrorBoundary} from 'react-error-boundary';
+import {ErrorFallBack} from '../components/ErrorFallBack';
 
 function App() {
- 
-
-return <>
+ return <>
     <Header />
     <table className={s.app}>
+    <ErrorBoundary
+     FallbackComponent={ErrorFallBack}
+     onReset={() => setUsers('')}>
       <Tbody />
+    </ErrorBoundary>
     </table>
   </>
 }
